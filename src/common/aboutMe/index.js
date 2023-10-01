12b10a342b019c7335468dc07700da422f30239d
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2023-10-01 20:34:32 +0200

    added elements to aboutMe component

--- src/common/aboutMe/index.js
+++ src/common/aboutMe/index.js
@@ -1,9 +1,26 @@
-import { StyledContainer, StyledPictureContainer } from "./styled";
+import { StyledContainer, StyledDescriptionContainer, StyledDescriptionParagraph, StyledPictureContainer } from "./styled";
 
 const AboutMe = () => (
 
     <StyledContainer>
         <StyledPictureContainer/>
+        <StyledDescriptionContainer>
+            <StyledDescriptionParagraph
+            fontSize={12}
+            color={"#6E7E91"}
+            fontWeight={700}
+            >Hello It's Me</StyledDescriptionParagraph>
+            <StyledDescriptionParagraph
+            fontSize={38}
+            color={"black"}
+            fontWeight={900}
+            >Paweł Leszczyński</StyledDescriptionParagraph>
+            <StyledDescriptionParagraph
+            fontSize={20}
+            color={"#6E7E91"}
+            fontWeight={400}
+            >I'm a passionate Frontend Developer. I'am really into storyteller websites that use 3D and paralax effects. Currently looking for new job opportunities.</StyledDescriptionParagraph>
+        </StyledDescriptionContainer>
     </StyledContainer>
 );
 export default AboutMe;
--- src/common/aboutMe/styled.js
+++ src/common/aboutMe/styled.js
@@ -1,17 +1,37 @@
 import styled from "styled-components";
+import myImage from "../../images/cv.png"
 
 export const StyledContainer = styled.div`
     background-color: aliceblue;
     min-width: 100%;
+    display: flex;
+    flex-direction: row;
+    gap: 30px;
     
 `;
 
 export const StyledPictureContainer = styled.div`
-    background-image: url("https://github.com/PaweLeszczynsky/Personal-Home-Page-React/blob/main/src/images/cv.png");
-    
-    height: 384px;
-    width: 384px;
+    background-image: url("${myImage}");
+    border-radius: 50%;
+    background-color: #a7a7a7;
+    min-height: 384px;
+    min-width: 384px;
     background-size: contain;
     background-position: center;
     background-repeat: no-repeat;
+`;
+export const StyledDescriptionContainer = styled.div`
+    height: 100%;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    gap: 10px;
+`;
+
+export const StyledDescriptionParagraph = styled.p`
+    font-weight: ${props=>props.fontWeight};
+    font-size: ${props=>props.fontSize}px;
+    color:${props=>props.color};
+    margin: 0;
 `;

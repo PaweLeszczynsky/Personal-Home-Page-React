a0f93c952f69e79a2862099cf1de44d5975a664a
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2023-10-03 20:16:59 +0200

    added new components to DOM

--- src/common/bodyContainer/index.js
+++ src/common/bodyContainer/index.js
@@ -1,11 +1,15 @@
 import AboutMe from "../aboutMe";
 import DarkLightModeButton from "../darkLightModeButton";
+import Skills from "../skills";
+import WhatLearnNext from "../whatLearnNext";
 import { StyledBodyContainer } from "./styled"
 
 const BodyContainer = () => (
     <StyledBodyContainer>
         <AboutMe />
         <DarkLightModeButton/>
+        <Skills/>
+        <WhatLearnNext/>
     </StyledBodyContainer>
 );
 export default BodyContainer;

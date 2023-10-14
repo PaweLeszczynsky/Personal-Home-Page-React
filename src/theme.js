20661de09ba30dbfe2ef10795092bf1fa43b0cfd
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2023-10-14 18:15:52 +0200

    added global styled, theme file and deleted css files

---
+++ src/theme.js
@@ -0,0 +1,43 @@
+export const LightTheme = {
+
+    color: {
+        background: "#FBFBFE",
+        font: "#6E7E91",
+        header: "#252525",
+        buttonThemeIconBackground: "#6E7E91",
+        buttonThemeBorder: "#6E7E91",
+        buttonThemeBackground: "#E5E5E5",
+        buttonThemeIconColor: "#FFFFFF",
+        sectionBackground: "#FBFBFE",
+        supplementItemsColor: "#18a59c",
+        repositoryBorderHover: "#1dc6bb4d"
+    },
+
+    mediaWidth: {
+        tablet: 1200,
+        smartphone: 768
+    }
+
+}
+
+export const DarkTheme = {
+
+    color: {
+        background: "#252525",
+        font: "#FFFFFF",
+        header: "#FFFFFF",
+        buttonThemeIconBackground: "#FFFFFF",
+        buttonThemeBorder: "#FFFFFF",
+        buttonThemeBackground: "#363636B8",
+        buttonThemeIconColor: "#252525",
+        sectionBackground: "#363636B8",
+        supplementItemsColor: "#1dc6bb",
+        repositoryBorderHover: "#1dc6bb80"
+
+    },
+
+    mediaWidth: {
+        tablet: 1200,
+        smartphone: 768
+    }
+}

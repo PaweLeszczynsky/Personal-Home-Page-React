746e7a15c3209a479d7f34794cf2c9b63bc9e094
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2023-10-14 18:24:11 +0200

    created component to describe skillset

--- src/common/list/styled.js
+++ src/common/list/styled.js
@@ -2,18 +2,25 @@ import styled from "styled-components";
 
 export const StyledListComponentContainer = styled.div`
     min-width: 100%;
-    background-color: white;
+    background-color: ${({ theme }) => theme.color.sectionBackground};
     box-shadow: 0px 16px 58px 0px #090A3308;
     border-radius: 4px;
     display: flex;
     flex-direction: column;
     gap: 10px;
     padding: 20px;
+    transition: .5s all;
 `;
 
 export const StyledListTitle = styled.h1`
     font-weight: 900;
     font-size: 30px;
+    color: ${({ theme }) => theme.color.header};
+    transition: all .5s;
+    @media (max-width:${({ theme }) => theme.mediaWidth.smartphone}px){
+        font-size: 18px;
+    }
+    
 
 `;
 export const StyledListUnderline = styled.div`
@@ -30,26 +37,45 @@ export const StyledListContainer = styled.div`
     
 `;
 export const StyledList = styled.ul`
-    padding-left: 20px;
+    padding-left: 0px;
     list-style: none;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-column-gap: 100px;
+    @media (max-width:${({ theme }) => theme.mediaWidth.tablet}px){
+        grid-template-columns: 1fr 1fr;
+    }
+    @media (max-width:${({ theme }) => theme.mediaWidth.smartphone}px){
+        grid-template-columns: 1fr;
+        margin: 0;
+    }
 `;
 
 export const StyledListItem = styled.li`
     font-size: 18px;
     font-weight: 400;
-    color: rgba(110, 126, 145, 1);
+    color: ${({ theme }) => theme.color.font};
     display: flex;
     align-items: center;
     letter-spacing: 1px;
     margin-top: 10px;
+    transition: all.5s;
+    @media (max-width:${({ theme }) => theme.mediaWidth.smartphone}px){
+        font-size: 14px;
+    }
     &::before{
         content: "";
         height: 9px;
         width: 9px;
         border-radius: 50%;
-        background-color:rgba(3, 102, 214, 1);
+        background-color:${({ theme }) => theme.color.supplementItemsColor};
         display: inline-block;
         margin-right: 10px;
+        transition: all.5s;
+        @media (max-width:${({ theme }) => theme.mediaWidth.smartphone}px){
+            height: 6px;
+            width: 6px;
+    }
     }
 
 `;
--- src/common/skills/index.js
+++ src/common/skills/index.js
@@ -1,25 +1,20 @@
 import { StyledListComponentContainer, StyledList, StyledListContainer, StyledListItem, StyledListTitle, StyledListUnderline } from "../list/styled";
 
-const Skills = () => (
-    <StyledListComponentContainer>
-        <StyledListTitle>My skillset includes</StyledListTitle>
-        <StyledListUnderline />
-        <StyledListContainer>
-            <StyledList>
-                <StyledListItem>StyledComponents</StyledListItem>
-                <StyledListItem>StyledIcons</StyledListItem>
-                <StyledListItem>Redux Saga</StyledListItem>
-            </StyledList>
-            <StyledList>
-                <StyledListItem>Teamwork</StyledListItem>
-                <StyledListItem>Scrum</StyledListItem>
-            </StyledList>
-            <StyledList>
-                <StyledListItem>Redux Saga</StyledListItem>
-                <StyledListItem>React Router</StyledListItem>
-            </StyledList>
-        </StyledListContainer>
-    </StyledListComponentContainer>
-);
+const Skills = () => {
+    return (
+        <StyledListComponentContainer>
+            <StyledListTitle>My skillset includes</StyledListTitle>
+            <StyledListUnderline />
+                <StyledList>
+                    <StyledListItem>StyledComponents</StyledListItem>
+                    <StyledListItem>StyledIcons</StyledListItem>
+                    <StyledListItem>Redux Saga</StyledListItem>
+                    <StyledListItem>Teamwork</StyledListItem>
+                    <StyledListItem>Scrum</StyledListItem>
+                    <StyledListItem>Redux Saga</StyledListItem>
+                    <StyledListItem>React Router</StyledListItem>
+                </StyledList>
+        </StyledListComponentContainer>)
+};
 
 export default Skills;

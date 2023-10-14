18874fb98ab0d0e80ec29cf31af14096d48d7bee
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2023-10-14 18:32:03 +0200

    implement themeProvider, globalStyles, darkLightState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { ThemeProvider } from 'styled-components';
 import './App.css';
 import BodyContainer from './common/bodyContainer';
+import { GlobalStyle } from './globalStyle';
+import { DarkTheme, LightTheme } from './theme';
+import { useDarkLightState } from './common/getDarkLightState';
 
-const App = () => (
-
-  <BodyContainer>
-    
-  </BodyContainer>
-
-
-);
+const App = () => {
+  const [darkLightState, changeVal] = useDarkLightState();
+  return (
+    <ThemeProvider theme={darkLightState ? DarkTheme : LightTheme}>
+      <GlobalStyle />
+      <BodyContainer changeVal={changeVal} darkLightState={darkLightState}/>
+    </ThemeProvider >
+  )
+};
 
 export default App;
